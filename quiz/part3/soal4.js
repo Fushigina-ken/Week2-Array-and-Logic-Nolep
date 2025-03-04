51343f0c9cfd0b54935512d29457750eb25e4852
Fushigina-ken <sn.aryasatya@gmail.com>
2025-03-04 22:45:09 +0700

    Tugas4 Part3 Week2

---
+++ quiz/part3/soal4.js
@@ -0,0 +1,26 @@
+function tentukanDeretGeometri(arr) {
+    // you can only write your code here!
+    if (arr.length < 2) return true;
+  
+  if (arr[0] === 0) {
+    return arr.every(num => num === 0);
+  }
+  
+  let ratio = arr[1] / arr[0];
+  
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i-1] * ratio !== arr[i]) {
+      return false;
+    }
+  }
+  
+  return true;
+
+  }
+  
+  // TEST CASES
+  console.log(tentukanDeretGeometri([1, 3, 9, 27, 81])); // true
+  console.log(tentukanDeretGeometri([2, 4, 8, 16, 32])); // true
+  console.log(tentukanDeretGeometri([2, 4, 6, 8])); // false
+  console.log(tentukanDeretGeometri([2, 6, 18, 54])); // true
+  console.log(tentukanDeretGeometri([1, 2, 3, 4, 7, 9])); // false
